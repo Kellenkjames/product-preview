b8001fa62cd11669303c63746e17cefb6ed705f5
Kellen James <8484636@ups.com>
2024-12-04 16:04:25 -0500

    feature: add data inputs to child components and bind properties

--- product-preview/src/app/image-display/image-display.component.ts
+++ product-preview/src/app/image-display/image-display.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-image-display',
   standalone: true,
   imports: [],
   templateUrl: './image-display.component.html',
-  styleUrl: './image-display.component.scss'
+  styleUrl: './image-display.component.scss',
 })
 export class ImageDisplayComponent {
-
+  @Input() desktopImageUrl!: string;
+  @Input() mobileImageUrl!: string;
 }
--- product-preview/src/app/product-info/product-info.component.ts
+++ product-preview/src/app/product-info/product-info.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Product } from '../product/model';
 
 @Component({
   selector: 'app-product-info',
   standalone: true,
   imports: [],
   templateUrl: './product-info.component.html',
-  styleUrl: './product-info.component.scss'
+  styleUrl: './product-info.component.scss',
 })
 export class ProductInfoComponent {
-
+  @Input() product!: Product;
 }
