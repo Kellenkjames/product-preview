193ea65fdfbe81eb6bd32c4032c31e2baa3c7e0d
Kellen James <8484636@ups.com>
2024-12-04 14:08:28 -0500

    feature: add components for image and content

--- product-preview/src/app/card/card.component.ts
+++ product-preview/src/app/card/card.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { ImageDisplayComponent } from '../image-display/image-display.component';
+import { ProductInfoComponent } from '../product-info/product-info.component';
 
 @Component({
   selector: 'app-card',
   standalone: true,
-  imports: [],
+  imports: [ImageDisplayComponent, ProductInfoComponent],
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.scss'],
 })
