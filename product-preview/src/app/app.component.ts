acfa0aeb0a84c9f7bd5fa4b3879c451144ef54c5
Kellen James <8484636@ups.com>
2024-12-04 13:00:59 -0500

    feature: add card component and remove Angular boilerplate

--- product-preview/src/app/app.component.ts
+++ product-preview/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { CardComponent } from './card/card.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [CardComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'product-preview';
